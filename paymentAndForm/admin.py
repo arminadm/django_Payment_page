c02112e24724c1a5c874dc7f52575e6af3dc0f16
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-04-17 21:22:40 +0430

    created customers model and admin panel settings for it

--- paymentAndForm/admin.py
+++ paymentAndForm/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from paymentAndForm.models import Customers
 # Register your models here.
+class CustomersAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ['name', 'lastname', 'email', 'payment', 'created_date']
+    list_filter = ('email','payment')
+    search_fields = ['name', 'lastname', 'email', 'phone']
+admin.site.register(Customers, CustomersAdmin)
--- paymentAndForm/models.py
+++ paymentAndForm/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Customers(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50)
+    lastname = models.CharField(null=False, blank=False, max_length=50)
+    address = models.TextField(null=False, blank=False,)
+    phone = models.IntegerField(null=False, blank=False, unique=True)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    payment = models.BooleanField(default=False)
+    class Meta:
+        verbose_name = ("Customer")
+        verbose_name_plural = ("Customers")
+        ordering = ['-created_date']
+    def __str__(self):
+        return self.email
